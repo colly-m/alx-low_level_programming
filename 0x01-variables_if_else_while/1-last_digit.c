5424d9cdac8422ebe22fa16815b63fbe75baa3ed
colly-m <sindanicolly@gmail.com>
2023-06-16 17:07:37 -0700

    Ending digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 /**
 *main - Entry point
-*Discription: "Get the last digits of a number"
+*Discription: "Get the last digits of a random number if it is
+*greater than 5, less than 6, or 0"
 *Return: always 0 (Success)
 */
 int main(void)
@@ -18,11 +19,11 @@ int main(void)
 	{
 		printf("last digit of %d is %d and is greater than 5\n", n, ld);
 	}
-	else if (ld == 0)
+	else
 	{
 		printf("last digit of %d is %d and is 0\n", n, ld);
 	}
-	else
+	else if (ld == 0)
 	{
 		printf("last digit of %d is %d and less than 6 and not 0\n", n, ld);
 	}
