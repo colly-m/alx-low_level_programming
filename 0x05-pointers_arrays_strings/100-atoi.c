453458d5d31a0cbebc2028443053b7838b7fb82d
colly-m <sindanicolly@gmail.com>
2023-06-26 12:54:23 -0700

    Sacrifices

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,16 +7,29 @@
 
 int _atoi(char *s)
 {
-	int i;
-	unsigned int num = 0;
+	int c = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
 
-	do {
-		if (*s == '-')
-		i *= -1;
-		else if (*s >= 48 && *s <= 57)
-		num = num * 10 + (*s - '0');
-		else if (num > 0)
-		break;
-	} while (*s++);
-	return (num * i);
+	while (s[c])
+	{
+		if (s[c] == 45)
+		{
+			min *= -1;
+		}
+		while (s[c] >= 48 && s[c] <= 57)
+		{
+			isi = 1;
+			ni = (ni * 10) + (s[c] - '0');
+			c++;
+		}
+		if (isi == 1)
+		{
+			break;
+		}
+		c++;
+	}
+	ni *= min;
+	return (ni);
 }
