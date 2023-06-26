c97e7aaa604bf37042681cc49eae63a2f6fc6a06
colly-m <sindanicolly@gmail.com>
2023-06-26 09:00:17 -0700

    Array not pointers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,11 +13,12 @@
 void print_array(int *a, int n)
 {
 	int index;
-
-	for (index = 0; index < n; index++)
+	
+	index = 0;
+	for (n--; n >= 0; n--, index++)
 	{
 		printf("%d", a[index]);
-		if (index != (n - 1))
+		if (n > 0)
 		{
 			printf(",");
 		}
