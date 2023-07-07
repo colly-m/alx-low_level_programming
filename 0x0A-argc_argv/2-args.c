bd4cc7cbd0651900749e62e30bc3da7e5aed34fd
colly-m <sindanicolly@gmail.com>
2023-07-07 06:20:40 -0700

    Every argument received

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program to print all received arguments and new line
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 always
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+	for (a = 0; a < argc; a++)
+	{
+		printf("%s\n", argv[a]);
+	}
+	return (0);
+}
