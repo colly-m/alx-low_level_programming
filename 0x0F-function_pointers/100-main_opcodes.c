1073b34e4a17a58bf84605ab9076ddd04184d79a
colly-m <sindanicolly@gmail.com>
2023-07-19 13:36:16 -0700

    Opps hacked

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - program to print opcodes of its own main
+ * @argc: number of argument passed
+ * @argv: array of pointers to the argument
+ * Return: 0 always
+ */
+int main(int argc, char **argv)
+{
+	int bytes, index;
+	unsigned char opcode;
+	int (*address)(int, char **) = main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Erroe\n");
+		exit(2);
+	}
+
+	for (index = 0; index < bytes; index++)
+	{
+		opcode = *(unsigned char *)address;
+		printf("%.2x", opcode);
+
+		if (index == bytes - 1)
+			continue;
+		printf(" ");
+
+		address++;
+	}
+	printf("\n");
+
+	return (0);
+}
