f7b32cdf002fb7e2c897fb20fddb83f06f82c5b6
colly-m <sindanicolly@gmail.com>
2023-07-27 12:52:27 -0700

    Hare and tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
 /**
- * funmain - function to print before the main function is executed
- * Return: Void
+ * funbmain - function to print before the main function is executed
+ *
  */
-void __attribute__((constructor)) funmain()
+void funbmain(void) __attribute__ ((constructor));
+
+/**
+ * funbmain - begining of funbmain
+ */
+void funbmain(void)
 {
-	printf("Youre beat! and yet, you must allow");
-	printf(",\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow\n");
+	printf("I bore my house upon my back!\n");
 }
