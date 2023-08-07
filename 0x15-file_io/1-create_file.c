e5fd23f95b17f0c3d581befbb552b06f88104634
colly-m <sindanicolly@gmail.com>
2023-08-07 14:31:42 -0700

    Snow white

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,13 +13,17 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-
+	fn = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fn == -1)
+		return (-1);
 	if (text_content != NULL)
 	{
-		for (count = 0; text_content[count] != '\0'; count++)
-			;
+		close(fn);
+		return (1);
 	}
-	fn = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	for (count = 0; text_content[count] != '\0'; count++)
+		;
+
 	wt = write(fn, text_content, count);
 
 	if (fn == -1 || wt == -1)
