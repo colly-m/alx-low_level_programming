e7d68e79fafe20513c9355cdbb5e55a4bf80f75e
colly-m <sindanicolly@gmail.com>
2023-12-20 07:25:00 -0800

    Surround land

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module to return perimeter of an island"""
+
+
+def island_perimeter(grid):
+    """
+    Calculate the perimeter of parameter described in the grid
+    Args:
+        grid (list of integers): REpresents island withn 0 for water and 1 for land
+    Return:
+        int: perimeter of the island
+    """
+    wid = len(grid[0])
+    ht = len(grid)
+    ejs = 0
+    sz = 0
+
+    for h in range(ht):
+        for w in range(wid):
+            if grid[h][w] == 1:
+                sz += 1
+                if (w > 0 and grid[h][w - 1] == 1):
+                    ejs += 1
+                if (h > 0 and grid[h - 1][w] == 1):
+                    ejs += 1
+    return (sz * 4 - ejs * 2)
